77cb5efddcd3c5a7ba1d4a4c5d75a6da85998cca
menslow <michael.enslow@gmail.com>
2011-04-04 23:57:58 -0700

    Initial app commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,11 @@
+class ProjectsController < ApplicationController
+
+  respond_to :html, :xml, :json
+
+  # GET /projects
+  # GET /projects.xml
+  def index
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mistermachine::Application.config.secret_token = '0e2c7f1cfc7b23b66a4bc44606ca05bddc10f409d08cffd979af7d3751ef66fc70e0eec3ccc995ac953f97171ca2a6cef38622ac44acdacef4731935194c6a9f'
